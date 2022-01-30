7535f9f318752ca0e68899c748cfc7ce3820f3a9
splitcomma <mozijegy@hotmail.com>
2022-01-30 19:58:21 +0000

    Adding print BMI category based on actual result value

--- bmi_claculator.py
+++ bmi_claculator.py
@@ -14,3 +14,13 @@ cm = float(input("Enter height(cm):"))
 # formatted float to 1f precision to print single decimal digit 
 bmi = kg / (cm / 100)**2
 print ("Your BMI index is:  {:.1f}" .format (bmi))
+
+# Bonus code for displaying BMI category based on the results
+if bmi < 18.5:
+    print("You are underweight, run to a restaurant while eating chocolate.")
+elif bmi <= 24.9:
+    print("You are healthy.")
+elif bmi <= 29.9:
+    print("You are overweight, what about some veggies instead of the deep fried junk food.")
+elif bmi >= 30.0:
+    print("You are obese, consider moving more and eating less.")
