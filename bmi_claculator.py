2d6d44701c858df5395e9a16b105e2fc3fcea202
splitcomma <mozijegy@hotmail.com>
2022-01-30 19:27:20 +0000

    Second version of BMI calculcator, corrected calculation and format input-output

--- bmi_claculator.py
+++ bmi_claculator.py
@@ -7,7 +7,9 @@
 # Enter height(cm): 180
 # The BMI is (kg/m2) 20.06.
 
-kg = input("Enter your weight in Kilograms:")
-cm = input("Enter height(cm):")
-bmi = int(kg) / int(cm)
-print("Your BMI index is:{}" .format (bmi))
+print ("This is a BMI calculator")
+kg = float(input("Please enter your weight in Kilograms:"))
+cm = float(input("Enter height(cm):"))
+
+bmi = kg / (cm / 100)**2
+print ("Your BMI index is:  {:.1f}" .format (bmi))
