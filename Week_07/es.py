a846d6d50a954579be8b266c519a8100385b0fc5
splitcomma <mozijegy@hotmail.com>
2022-03-15 21:20:00 +0000

    letter count added

--- Week_07/es.py
+++ Week_07/es.py
@@ -6,9 +6,17 @@
 # The program should take the filename from an argument on the command line.
 
 
+file = open('Week_07\mobydick.txt', 'r')
 
+mobiDick= file.read()
 
+letter1 = 'e'
+letter2 = 'E'
 
+letterCount = mobiDick.count(letter1) + mobiDick.count(letter2)
+print(letterCount)
+
+ 
 
 
 # Used references:
